bf17c8779d33d54616622f147dc007f53904d941
Wuyu-byte <hunanpyy@163.com>
2023-08-03 12:49:01 +0800

    test

---
+++ settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+SECRET_KEY = "asghnjadfbtywyn"
+
+# openAi api key
+# OPENAI_API_KEY = "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"
+
+# openAi 官方 api
+URL = "https://api.openai.com/v1/chat/completions"
+
+# openAi 代理 api
+# URL = "https://open.aiproxy.xyz/v1/chat/completions"
